775ebc1d979934652d459584d7a740ae9f10d5c6
Md. Mahbub Alam <mmahbub569@gmail.com>
2024-05-22 12:38:58 +0600

    Bootstrap UI and form validation updated

--- appointmentSchedulerUI/src/app/components/login/login.component.ts
+++ appointmentSchedulerUI/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateform';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa-eye-slash';
+
+  loginForm!:FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.loginForm = this.fb.group({
+      userName: ['', Validators.required],
+      passWord: ['', Validators.required]
+    })
+  }
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.type = 'text' : this.type = 'password';
+    this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash';
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log('user ',this.loginForm.value);
+      
+    }
+    else{
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert('User is not valid!')
+    }
   }
 
 }
--- appointmentSchedulerUI/src/app/components/signup/signup.component.ts
+++ appointmentSchedulerUI/src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateform';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa-eye-slash';
+
+  signUpForm!:FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      userName: ['', Validators.required],
+      passWord: ['', Validators.required]
+    })
+  }
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.type = 'text' : this.type = 'password';
+    this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash';
+  }
+
+  onSubmit(){
+    if(this.signUpForm.valid){
+      console.log('user ',this.signUpForm.value);
+      
+    }
+    else{
+      ValidateForm.validateAllFormFields(this.signUpForm);
+      alert('User is not valid!')
+    }
   }
 
 }
